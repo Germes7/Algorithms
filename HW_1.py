7b28d41de83b1ca68d7b8b9a1a0a87d6ea31f089
Derek <ushakov_gd@mail.ru>
2026-04-19 15:14:10 +0300

    Написал решение 3 задаачи

--- HW_1.py
+++ HW_1.py
@@ -1,3 +1,4 @@
+# Задача №1
 # Необходимо разработать алгоритм, который принимает на вход список целых
 # чисел и возвращает сумму всех четных чисел в этом списке. Если сумма
 # отрицательная, вернуть 1.
@@ -10,26 +11,27 @@
 # Список целых чисел, каждое число в диапазоне от -2 * 10**4 до 2 * 10**4.
 # Формат: последовательность целых чисел типа list.
 
-# def sum_even_numbers(arr: list[int]) -> int:
-#
-#     if not arr:
-#         return 0
-#
-#     if not isinstance(arr, list): raise TypeError("Нужен массив целых чисел")
-#
-#     sum_even = 0
-#     for i in arr:
-#         if not isinstance(i, int): raise TypeError("Числа должны быть натуральными")
-#
-#         if i % 2 == 0:
-#             sum_even += i
-#
-#     if sum_even < 0:
-#         return 1
-#
-#     return sum_even
+def sum_even_numbers(arr: list[int]) -> int:
+
+    if not arr:
+        return 0
+
+    if not isinstance(arr, list): raise TypeError("Нужен массив целых чисел")
+
+    sum_even = 0
+    for i in arr:
+        if not isinstance(i, int): raise TypeError("Числа должны быть натуральными")
+
+        if i % 2 == 0:
+            sum_even += i
+
+    if sum_even < 0:
+        return 1
+
+    return sum_even
 
 
+# Задача №2
 # Разработайте алгоритм, который определяет наиболее часто встречающийся
 # элемент в предложенном списке чисел. Если таких элементов несколько, верните наименьший из них.
 
@@ -68,9 +70,39 @@ def repeating_number(arr: list[int]) -> int:
     return repeat_elem
 
 
-arr = [1, 5, 2, 2, 1, 5, 0, 3, 1, 4, 5, -7, 2, 5]
-print(repeating_number(arr))
-# sort = [-7, 0, 1, 1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 5]
-# count = 4
-# prim_count = 3
-# repeat_elem = 5
+# Задача №3
+# Дан массив целых чисел nums и целое число target.
+# Верните индексы этих двух чисел так, чтобы их сумма равнялась target.
+# Предположим, что для каждого входного значения существует ровно одно решение, и нельзя использовать
+# один и тот же элемент дважды. Ответ можно возвращать в любом порядке.
+
+# Пример №1
+# Входные данные: nums = [2,7,11,15], target = 9
+# Выходные данные: [0, 1]
+# Пояснение: Поскольку nums[0] + nums[1] == 9, мы возвращаем [0, 1]
+
+# Пример №2
+# Входные данные: nums = [3, 2, 4], target = 6
+# Выходные данные: [1, 2]
+
+#VAR 1
+def output_iter(sequence: list[int], target: int)-> list[int]:
+
+    if not sequence:
+        return 0
+
+    if not isinstance(sequence, list): raise TypeError("Нужен массив целых чисел")
+
+    lengts = len(sequence)
+
+    for i in range(lengts):
+        for j in range(i + 1, lengts):
+            if sequence[i] + sequence[j] == target:
+                return [i, j]
+
+    return []
+
+
+arr = [8, -5, 7, 1, 6, 9]
+tar = 7
+print(output_iter(arr, tar))
