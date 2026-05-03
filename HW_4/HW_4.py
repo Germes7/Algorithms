e582858ef53ef5c6431e17b2d0fdd993eeb6ab44
Derek <ushakov_gd@mail.ru>
2026-05-03 21:17:29 +0300

    Написал решение Задачи №3

--- HW_4/HW_4.py
+++ HW_4/HW_4.py
@@ -4,6 +4,7 @@ sys.set_int_max_str_digits(10000000)
 import matplotlib.pyplot as plt
 import random
 from typing import Any
+sys.setrecursionlimit(10000)
 
 # Задача №1. Сортировка “Пузырька”
 # Напишите функцию, которая принимает на вход массив элементов, анонимную функцию key (задает ключ сортировки)
@@ -96,6 +97,45 @@ if __name__ == "__main__":
     plt.plot(n_values, times, marker='o', linestyle='-', color='b')
 
     plt.title('Тест сортировки методом "Выбора"')
+    plt.xlabel('Количество элементов (n)')
+    plt.ylabel('Время выполнения (секунды)')
+    plt.grid(True)
+    plt.show()
+
+
+# Задача №3.
+# Напишите функцию recursive_sum, которая принимает на вход массив элементов и возвращает сумму
+# всех элементов массива. Если переданный на вход массив пустой, функции должны возвращать 0.
+# Используйте рекурсивный подход.
+
+def recursive_sum(arr: list[int | float]) -> int | float:
+    if not isinstance(arr, list): raise TypeError("Нужен список")
+    if not arr: return 0
+
+    sum = arr[0]
+    del arr[0]
+    return sum + recursive_sum(arr)
+
+
+# O(n) = 1 + 1 + 1 + (n2) => O(n2)
+
+if __name__ == "__main__":
+
+    n_values = [10, 50, 100, 200, 400, 500, 1000, 2000, 4000, 7000, 9000]
+    times = []
+
+    for n in n_values:
+        test_arr = [random.randint(0, 1000) for _ in range(n)]
+        start_time = time.perf_counter()
+        recursive_sum(test_arr)
+        end_time = time.perf_counter()
+
+        times.append(end_time - start_time)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_values, times, marker='o', linestyle='-', color='b')
+
+    plt.title('Тест функции sum с рекурсией')
     plt.xlabel('Количество элементов (n)')
     plt.ylabel('Время выполнения (секунды)')
     plt.grid(True)
